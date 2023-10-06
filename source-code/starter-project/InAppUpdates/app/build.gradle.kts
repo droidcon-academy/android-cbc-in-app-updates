4b35e274bd5ff1e780d990064ad6c270d1417dde
LethalMaus <descriptiveanimals@gmail.com>
2023-10-06 10:38:33 +0200

    Updated build gradle deps

--- source-code/starter-project/InAppUpdates/app/build.gradle.kts
+++ source-code/starter-project/InAppUpdates/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -38,6 +39,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
